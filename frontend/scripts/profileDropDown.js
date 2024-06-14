d4e849b88f10efef488ed46a39bb2152c10ab8d9
cal20014 <cal20014@byui.edu>
2024-06-14 16:28:23 -0600

    Update profile section and navigation styles in frontend dashboard.html and main.css

---
+++ frontend/scripts/profileDropDown.js
@@ -0,0 +1,14 @@
+const profileDropDown = document.querySelector("#profile-section");
+
+const profileDropDownContent = `<div class="profile-dropdown-content">
+    <a href="profile.html">Manage Profile</a>
+    <a href="Settings" </a>
+</div>`;
+
+const profileDropDownMenu = () => {
+  profileDropDown.innerHTML = profileDropDownContent;
+};
+
+const profileDropDownHandler = () => {
+  profileDropDown.addEventListener;
+};
